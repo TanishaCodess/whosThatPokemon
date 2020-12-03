5b66942cf4978a3e1db394a8abc237a117f51ff1
Lezgo7 <tanishaminson@gmail.com>
2020-12-03 15:56:20 -0500

    cleaned up code, edited comments, only highscore left to do

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 let pokeName = "";
 let pokeNameJumbled = "";
 
-// make the API call a function so I can call for a new word without refreshing the page
+// function that calls the Pokemon API to retrieve a random pokemon name
 const getPokemonName = function() {
-    //make a call to pokemon API using ID
     fetch(
             // generate a random pokemon name using randomized ID# (API has pokemon 1-898 but Im only using 1-151)
             `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 151 + 1)}/`
@@ -15,12 +14,14 @@ const getPokemonName = function() {
         .then(function(data) {
             //real name of Pokemon from API
             const pokeName = data.name;
+            console.log(pokeName);
             // jumbled name of Pokemon
             const pokeNameJumbled = pokeName.shuffle();
             //function to display the jumbled name and bring the real name out of the fetch scope
             displayInfomation(pokeName, pokeNameJumbled);
         });
 };
+getPokemonName();
 
 //function to display jumbled name
 const displayInfomation = function(name, jumbledName) {
@@ -30,14 +31,10 @@ const displayInfomation = function(name, jumbledName) {
     domJumble.textContent = jumbledName;
 };
 
-// Function for again button to get another name to guess
-const tryAgain = function() {
-    // reset the input
-    document.querySelector(".guess").value = "";
-    // generate new jumbled word/display
-    getPokemonName();
-};
-
+// score and highscore variables at default
+let score = 20;
+let highscore = 0;
+document.querySelector(".score").textContent = score;
 // Function to check the guess against the real name
 const checkName = function() {
     // make sure its all lowercase
@@ -57,12 +54,22 @@ const checkName = function() {
         document.querySelector(".score").textContent = score;
     }
 };
-getPokemonName();
 
 // on click of check button compare user input 'guess' against answer
 document.querySelector(".check").addEventListener("click", function() {
     checkName();
 });
+
+// Function for again button to display a new jumbled word to guess
+const tryAgain = function() {
+    // reset the input
+    document.querySelector(".guess").value = "";
+    //reset score
+    score = 20;
+    document.querySelector(".score").textContent = score;
+    // generate new jumbled word/display it
+    getPokemonName();
+};
 
 // Again BUTTON
 document.querySelector(".again").addEventListener("click", function() {
